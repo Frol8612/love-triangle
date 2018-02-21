e40e8fbf475134cab3703dd80e63f1b8f22632fb
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-02-21 19:19:52 +0300

    love

--- src/index.js
+++ src/index.js
@@ -4,4 +4,33 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let sum = 0,
+      k = [],
+      arr = [],
+      j = 0;
+        
+  for (let i = 0; i < preferences.length; i++) {
+    let n = i + 1;
+    k.push(n);
+  }
+
+  while (j<k.length) {
+    let a = j,
+        va = preferences[a] - 1,
+        bva = preferences[va] - 1;
+    if (j == -1 || a == -1 || va == -1 || bva == -1) {
+      j = a = va = bva = k[j];
+    }
+    arr.push(k[j], preferences[a], preferences[va], preferences[bva]);
+    let f = arr.filter(function(x) {
+      return x = k[j] - preferences[bva];
+    });
+    if (f == 0) {
+      sum++;
+    }
+    j++;
+  }
+  let s = sum/3;
+  return Math.floor(s);
+      
 };
