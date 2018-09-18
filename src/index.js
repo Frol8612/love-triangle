178506bb7a1ac8e6bfe3d73a758452acad93dcb5
Frol8612 <infern00730@gmail.com>
2018-09-18 11:23:54 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -3,48 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-  let sum = 0,
-      k = [],
-      arr = [],
-      j = 0;
-        
-  for (let i = 0; i < preferences.length; i++) {
-    let n = i + 1;
-    k.push(n);
-  }
+    // your implementation
+    let numTriangles = 0;
+    let arrLove = [].concat(preferences);
 
-  while (j < k.length) {
-    let a = j,
-        va = preferences[a] - 1,
-        bva = preferences[va] - 1;
-    if (j == -1 || a == -1 || va == -1 || bva == -1) {
-      j = a = va = bva = k[j];
-    }
-    arr.push([k[j], preferences[a], preferences[va], preferences[bva]]);
-    let f = arr.filter(function(x) {
-      if (preferences[va] - preferences[bva] == 0) {
-        return 1;
-    } else {
-        return x = k[j] - preferences[bva];
-    }
+    arrLove.forEach((el, i, arr) => {
+        let loveTriangle = arr[arr[el - 1] - 1];
+        if (loveTriangle === i + 1) {
+            numTriangles++;
+        } 
     });
-    if (f == 0) {
-      sum++;
-    }
-    j++;
-  }
 
-  let s;
-    if(sum < 3) {
-        s = sum;
-    } else {
-        s = sum/3;
-    }
-    if (s == 30) {
-      s = 31;
-    }
-
-  return Math.ceil(s);
-      
+    return ~~(numTriangles / 3);
 };
